053ec8e5183dd89e48c4dde9f42e0cede1997563
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-08 09:28:13 +0800

    crawler

--- pachong/test.py
+++ pachong/test.py
@@ -1,10 +1,15 @@
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
-# 这里会有两种异常：网页不存在（404）或者服务器异常（500）
+# 这里会有两种异常：网页不存在（404）或者服务器不存在（urlopen返回一个None）
+
 try:
     html = urlopen("http://www.sivan0222.cn")
-    bsObj = BeautifulSoup(html.read())
+    if html is None:
+        print("url is not found")
+    else:
+        bsObj = BeautifulSoup(html.read())
+
 except HTTPError as e:
     print(e)
     # 返回空值，执行另一种方案或者终止
