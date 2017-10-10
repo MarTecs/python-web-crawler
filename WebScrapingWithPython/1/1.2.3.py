e885f88b3a00e95c9ba4583553d56708f8651fb4
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-10 12:25:28 +0800

    add files

--- WebScrapingWithPython/1/1.2.3.py
+++ WebScrapingWithPython/1/1.2.3.py
@@ -5,9 +5,12 @@ url = "http://www.baidu.com"
 
 try:
     html = urlopen(url)
+    if html is None                 # 这里当采用urlopen函数打开一个url的时候，如果Url不存在将会返回一个None
+        print("Url is not found")
 except HTTPError as e:
     print(e)
 else:
+
 
 bsObj = BeautifulSoup(html.read())
 print(bsObj.head.title)
