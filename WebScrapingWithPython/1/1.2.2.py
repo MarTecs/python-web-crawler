809bb36198c0d997583ce73fdf4ea5d705f31909
sivanWu0222 <sivan0222@vip.qq.com>
2018-05-18 11:13:05 +0800

    Init

--- WebScrapingWithPython/1/1.2.2.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-url = "http://www.baidu.com"
-html = urlopen(url)
-bsObj = BeautifulSoup(html.read())
-print(bsObj.head.title)
-
--- WebScrapingWithPython/2/2/__init__.py
+++
@@ -1,5 +0,0 @@
-from urllib import urlopen
-from bs4 import BeautifulSoup
-html = urlopen("http;//www.pythonscraping.com/pages/warandpeace.html")
-bsObj = BeautifulSoup(html.read())
-
